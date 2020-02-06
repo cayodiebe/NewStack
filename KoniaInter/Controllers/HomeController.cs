f09a3c85721444caecc5f5e4f294640348224baf
Cayo Diebe <cayo.diebe@konia.com.br>
2020-02-05 23:57:46 -0300

    Inicial Commit

---
+++ KoniaInter/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using KoniaInter.Models;
+
+namespace KoniaInter.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Calculo()
+        {
+            Calculo calculo = new Calculo();
+
+            calculo.valor2 = 10;
+            calculo.valor1 = 10;
+            calculo.operacao = "+";
+            ViewData["Total"] = calculo.CalculaValores(calculo);
+            return View();
+        }
+
+        //##Será Necessário Criar uma nova IAccountResult para a Home de Calculo##
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
+;
---
+++ KoniaInter/Models/Calculo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KoniaInter.Models
+{
+    public class Calculo
+    {
+        public double valor1 { get; set; }
+        public double valor2 { get; set; }
+        public string operacao { get; set; }
+
+        public double CalculaValores(Calculo calculo)
+        {
+            double total = 0;
+            if (calculo.operacao == "+")
+                total = calculo.valor1 + calculo.valor2;
+
+            else if (calculo.operacao == "-")
+                total = calculo.valor1 - calculo.valor2;
+            else if (calculo.operacao == "*")
+                total = calculo.valor1 * calculo.valor2;
+            else if (calculo.operacao == "/")
+                total = calculo.valor1 / calculo.valor2;
+
+            return total;
+        }
+    }
+}
